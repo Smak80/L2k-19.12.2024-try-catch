994a0c853d41c3c8c10c3cea490c34a5c43a4dc7
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-12-19 12:15:21 +0300

    Добавьте файлы проекта.

---
+++ L2k-19.12.2024-try-catch/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine(1.0 / 0.0);
+var a = 3;
+//var b = 3 - 3;
+//Console.WriteLine(a / b);
+//Console.WriteLine("Конец");
+
+Person p = new Person();
+while(true)
+    try
+    {
+        p.Age = int.Parse(Console.ReadLine());
+        Console.WriteLine(p.Age);
+        break;
+    } 
+    catch
+    {
+        Console.WriteLine("Введен неверный возраст. Повторите ввод");
+    }
+
+class Person
+{
+    private int _age;
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (value >= 0) _age = value;
+            else throw new Exception("Возраст должен быть положительной величиной");
+        }
+    }
+}
