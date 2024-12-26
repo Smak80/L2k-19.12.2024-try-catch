d5b0ed166d8d217d5491a0563621a67b5809d94e
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-12-26 10:55:21 +0300

    Исключения (пполностью)

--- L2k-19.12.2024-try-catch/Program.cs
+++ L2k-19.12.2024-try-catch/Program.cs
@@ -5,18 +5,31 @@ var a = 3;
 //Console.WriteLine("Конец");
 
 Person p = new Person();
-while(true)
+while (true)
+{
     try
     {
         p.Age = int.Parse(Console.ReadLine());
         Console.WriteLine(p.Age);
         break;
-    } 
+    }
+    catch (AgeException e)
+    {
+        Console.WriteLine(e.Message + " Повторите ввод");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Введено не целое число! Повторите ввод");
+    }
     catch
     {
-        Console.WriteLine("Введен неверный возраст. Повторите ввод");
+        Console.WriteLine("Что-то пошло не так :(");
     }
-
+    finally
+    {
+        Console.WriteLine("------");
+    }
+}
 class Person
 {
     private int _age;
@@ -25,8 +38,33 @@ class Person
         get => _age;
         set
         {
-            if (value >= 0) _age = value;
-            else throw new Exception("Возраст должен быть положительной величиной");
+            var f = new FileStream("somefile.txt", FileMode.CreateNew);
+            try
+            {
+                if (value >= 0)
+                {
+                    _age = value;
+                    f.Write(new byte[] { 23 });
+                }
+                else throw new AgeException(value);
+            }
+            finally
+            {
+                f.Close();
+            }
+
         }
     }
+}
+
+class AgeException : Exception
+{
+    private int _incorrectAgeValue;
+    public AgeException(int age) : 
+        base($"Значение {age} является некорректным. " +
+              "Укажите положительное число")
+    {
+        _incorrectAgeValue = age;
+    }
+
 }
